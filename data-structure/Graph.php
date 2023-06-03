4eadbfae12931f9b44b86e009de8b99f5de68acc
tarekmonjur <tarekmonjur@gmail.com>
2023-06-03 20:47:03 +0600

    completed shorted path graph

--- data-structure/Graph.php
+++ data-structure/Graph.php
@@ -45,7 +45,7 @@ class Graph {
     public function traverseByBFS($from, $to)
     {
         if (!$this->adjacency[$from] && !$this->adjacency[$to]) {
-            echo "from and to not found on the graph";
+            echo "from and to not found on the graph \n";
             return false;
         }
 
@@ -77,9 +77,62 @@ class Graph {
         return [];
     }
 
-    public function shortestPath()
+    public function shortestPath($source, $target)
     {
+        if (!isset($this->adjacency[$source]) || !isset($this->adjacency[$target])) {
+            echo "source and target not found on the graph \n";
+            return false;
+        }
 
+        $queue = new SplPriorityQueue();
+        $distance = [];
+        $previous = [];
+
+        foreach ($this->adjacency as $v => $adjacency) {
+            foreach($adjacency as $vertex => $value) {
+                if ($vertex == $source) {
+                    $queue->insert($vertex, 0);
+                } else {
+                    $queue->insert($vertex, -$value);
+                }
+            }
+            $distance[$v] = INF;
+            $previous[$v] = null;
+        }
+
+        $distance[$source] = 0;
+
+        while (!$queue->isEmpty()) {
+            $vertex = $queue->extract();
+            if ($vertex == $target) {
+                break;
+            }
+            // echo $vertex."\n";
+            foreach($this->adjacency[$vertex] as $vtx => $value) {
+                $alter = $distance[$vertex] + $value;
+                // echo $distance[$vertex]."+".$value."=".$alter."<".$distance[$vtx]."->".$vtx."\n";
+                if ($alter < $distance[$vtx]) {
+                    // echo "vertex=". $vertex.", alt=". $alter.", vtx=".$vtx.", value=".$value.", dis=".$distance[$vtx]."\n";
+                    $distance[$vtx] = $alter;
+                    $previous[$vtx] = $vertex;
+                }
+            }
+        }
+        
+        $temp = $target;
+        $path = [];
+        $dist = 0;
+
+        while($temp != null) {
+            if ($temp) {
+                $path[$temp] = $temp;
+                $dist += $this->adjacency[$temp][$previous[$temp]] ?? 0;
+            }
+            $temp = $previous[$temp];
+        }
+
+        $path = array_reverse($path);
+        printf("source %s to %s target shortest path is: %s : %d \n", $source, $target, implode('->', $path), $dist);
     }
 }
 
@@ -128,7 +181,7 @@ $graph->traverseByBFS('A', 'J');
 echo "\n \n";
 
 $graph = new Graph(['A', 'B', 'C', 'D', 'E', 'F']);
-// directed graph
+// weighted graph
 $graph->addEdge('A', 'B', 3);
 $graph->addEdge('A', 'D', 3);
 $graph->addEdge('A', 'F', 6);
@@ -150,5 +203,7 @@ $graph->addEdge('F', 'C', 3);
 $graph->addEdge('F', 'D', 2);
 $graph->addEdge('F', 'E', 5);
 $graph->displayGraph();
-// print_r($graph);
-// $graph->traverseByBFS('A', 'J');
+$graph->shortestPath('D', 'C');
+$graph->shortestPath('C', 'A');
+$graph->shortestPath('F', 'A');
+$graph->shortestPath('A', 'M');
